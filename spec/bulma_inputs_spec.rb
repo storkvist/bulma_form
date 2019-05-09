812417825b7eb2aac13f0ab404f676eb4761edab
Vitaly Shlyaga <vitaly@shlyaga.ru>
2019-05-09 14:38:38 +0300

    specs for number_field, password_field, text_field

--- spec/bulma_inputs_spec.rb
+++ spec/bulma_inputs_spec.rb
@@ -18,4 +18,43 @@ describe 'Input elements' do
 
     expect(builder.email_field(:misc)).to be_the_same_xml_as(expected)
   end
+
+  it 'renders correct number fields' do
+    expected = <<-HTML.strip_heredoc
+      <div class="field">
+        <label class="label" for="user_misc">Misc</label>
+        <div class="control">
+          <input class="input" type="number" id="user_misc" name="user[misc]">
+        </div>
+      </div>
+    HTML
+
+    expect(builder.number_field(:misc)).to be_the_same_xml_as(expected)
+  end
+
+  it 'renders correct password fields' do
+    expected = <<-HTML.strip_heredoc
+      <div class="field">
+        <label class="label" for="user_misc">Misc</label>
+        <div class="control">
+          <input class="input" type="password" id="user_misc" name="user[misc]">
+        </div>
+      </div>
+    HTML
+
+    expect(builder.password_field(:misc)).to be_the_same_xml_as(expected)
+  end
+
+  it 'renders correct text fields' do
+    expected = <<-HTML.strip_heredoc
+      <div class="field">
+        <label class="label" for="user_misc">Misc</label>
+        <div class="control">
+          <input class="input" type="text" id="user_misc" name="user[misc]">
+        </div>
+      </div>
+    HTML
+
+    expect(builder.text_field(:misc)).to be_the_same_xml_as(expected)
+  end
 end
